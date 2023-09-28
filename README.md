5ac69a9b5d11ca36f833dec1893ecda3f8695d2f
Patrick Reh <patrick.reh@thinkparq.com>
2023-09-28 09:46:02 +0200

    Add Rust generation and change output structure
    * Generate Rust modules along with the necessary structure and
      Cargo.toml to allow importing it as dependency. Uses the protoc-rs tool
      from https://github.com/thinkparq/protoc-rs.
    * To adapt to the requirements of the Rust generation, change the output
      structure for all languages. The generated files are now put into one
      top level subfolder per language, e.g. go/, cpp/, rust/
    * Fix some issues with the makefile
    * Update the readme
    
    Adapt to protoc-rs 0.2.0

--- README.md
+++ README.md
@@ -28,6 +28,7 @@ Languages with precompiled/generated code:
 | -------- | ------------ | ------------------------------------------------------------------------------------------ |
 | Go       | Yes          | N/A                                                                                        |
 | C++      | Yes          | A target has been added to the Makefile, but the resulting files have not been tested yet. |
+| Rust     | Yes          | A target has been added to the Makefile, but the resulting files have not been tested yet. |
 
 # Quick Start
 
@@ -40,7 +41,7 @@ appropriate for their language.
 As long as this repository is private, some additional configuration is required
 to allow this repository to be imported as a private module into your Go
 project. From there you can use Go Modules and all of the normal tools such as
-`go get`, `go mod tidy`, and `go mod vendor` to manage the `bee-protos` as a
+`go get`, `go mod tidy`, and `go mod vendor` to manage the `protobuf` as a
 dependency of your project. 
 
 IMPORTANT: These steps presume you are using SSH and the SSH key for the machine
@@ -53,7 +54,7 @@ Steps:
 access the repository directly instead of going through the central services
 (which could potentially leak information, such as the project name):
 ```shell
-export GOPRIVATE=github.com/thinkparq/bee-protos/go
+export GOPRIVATE=github.com/thinkparq/protobuf/go
 ```
 (2) Add the following to your global `.gitconfig` file to ensure `go get` will
 use SSH instead of HTTP:
@@ -61,10 +62,10 @@ use SSH instead of HTTP:
 [url "ssh://git@github.com/"]
 	insteadOf = https://github.com/
 ```
-(3) From here you can add any of the packages from bee-protos as a dependency
+(3) From here you can add any of the packages from protobuf as a dependency
 with `go get`. For example to use BeeWatch in your project run `go get
-github.com/thinkparq/bee-protos/beewatch/go`. When changes are made to the
-`bee-protos` project you can update dependencies with `go get -u <URL>` 
+github.com/thinkparq/protobuf/go/beewatch`. When changes are made to the
+`protobuf` project you can update dependencies with `go get -u <URL>`
 
 # Advanced: Generate / Compile `.proto` Files
 
@@ -77,18 +78,32 @@ intended for you.
 When working with protobuf files and vscode it is recommended to install [this
 extension](https://marketplace.visualstudio.com/items?itemName=zxh404.vscode-proto3).
 Especially if you are using a workspace, you will need to add the absolute path
-to the bee-protos project in either your user or workspace `settings.json` file:
+to the protobuf project in either your user or workspace `settings.json` file:
 
 ```
     "protoc": {
         "options": [
-            "--proto_path=/home/joe/development/beeflex/bee-protos/.",
+            "--proto_path=/home/joe/development/protobuf/.",
         ],
     },
 ```
 
+## Important limitations when adding new .proto files
+
+Protobuf allows naming a package differently than the file it is placed in. This, however, is not
+allowed here. The `package` directive within the proto files **must** have the same name as the file
+itself and can only contain alphanumeric characters and underscores (especially no `.`).
+
+The reasons are:
+* The Rust protobuf compiler uses the package name for naming the output file, which then could
+  potentially be named different than the input file. Which we do not want to avoid confusion.
+* Rust module names cannot contain `.` and protobuf packages are translated into Rust modules.
+
 ## Prerequisites
 
+* Necessary development tools and / or compiler of the supported languages.
+    * For Rust: https://rustup.rs/
+    * For Go: https://go.dev/doc/install
 * Install the Protocol buffer compiler `protoc` version 3.
   * Generally it is recommended to install the [pre-compiled
     binaries](https://grpc.io/docs/protoc-installation/#install-pre-compiled-binaries-any-os)
@@ -98,42 +113,41 @@ to the bee-protos project in either your user or workspace `settings.json` file:
   with gRPC.
   * For example [for Go](https://grpc.io/docs/languages/go/quickstart/) you need
     `protoc-gen-go` and `protoc-gen-go-grpc`.
+* Install the custom Rust protocol buffer compiler from https://github.com/thinkparq/protoc-rs.
+  This can be done using `cargo`:
+  ```
+  cargo install --git "https://github.com/thinkparq/protoc-rs"
+
+  ```
 * Ensure all the necessary binaries are added to your `$PATH` variable using
-  `.bashrc` or similar.
+  `.bashrc` or similar. For `cargo`, the install directory is usually `~/.cargo/bin`.
 
 ## Generating Code for a New Language
 
-The project Makefile is setup with targets to compile all protobuf files in the
-project into a multiple target languages when `make` or `make protos` is
-executed.
+The project Makefile is setup with targets to compile all protobuf files under the `./proto`
+directory into a multiple target languages when `make` or `make protos` is
+executed. Each languages output is put into a separate output directory (e.g. `./go`, `./rust`,
+`./cpp`).
 
 To add a new language amend the Makefile as follows:
 
-1. In each directory containing a `.proto` file add a new subdirectory where
-   language specific files will be compiled/generated (for example `cpp` or
-   `go`).
-1. Add any additional options to the `.proto` files required by the language.
+1. Define the output directory of the new language by adding a new <LANGUAGE>_OUT_DIR variable
+2. Add any additional options to the `.proto` files required by the language.
    1. For example for Go something like `option go_package =
-      "github.com/thinkparq/bee-protos/beesync/go";` is required so cross
+      "github.com/thinkparq/protobuf/go/beewatch";` is required so cross
       package imports work correctly.
-2. Add a new variable `<LANGUAGE>_GENERATED_FILES` that transforms the `.proto`
+3. Add a new variable `<LANGUAGE>_GENERATED_FILES` that transforms the `.proto`
    file names returned by `PROTO_FILES` into corresponding files to generate for
-   the target language.
-   1. For example for Go this looks like: `GO_GENERATED_FILES := $(patsubst
-      %.proto, %.pb.go, $(PROTO_FILES))`.
-3. Modify the `protos` target to add the generated list of files as a
+   the target language within the correct output directory. 
+4. Modify the `protos` target to add the generated list of files as a
    dependency.
-4. Create a new pattern rule for the target language to generate the appropriate
-   source files from the `.proto` files with the correct `protoc` command for
-   the new language. Ensure to any `_out` flags are appended with the
-   appropriate directory where language specific files will be
-   generated/compiled (for example `cpp` or `go`).
-   1. For example for Go this rule looks like: 
-```Makefile
-%.pb.go: %.proto
-	@echo "Compiling Go $<"
-	protoc -I $(dir $<) -I. --go_out=$(dir $<)/go --go_opt=paths=source_relative --go-grpc_out=$(dir $<)/go --go-grpc_opt=paths=source_relative $<
-```
+5. Create a new pattern rule for the target language to generate the appropriate
+   source files from the `.proto` files with the correct `protoc` (or other) command for
+   the new language in the correct output directory. Ensure to any `_out` flags or similar are set to the
+   correct language specific output directory (can be extracted from the target file using `$(dir $@)`).
+6. Add the new output directory to the find command in the `test` target.
+
+In general, you can likely just copy the code for one of the existing languages and modify it to your needs.
 
 ## Generating Code for Golang
 
@@ -143,6 +157,14 @@ want to manually generate the code you will need to
 compiler v3 (protoc) and Go plugins for the protocol compiler. Whenever changes
 are made to the `*.protos` files ensure to run `make protos` and commit the
 changes to the repository. 
+
+## Generating Code for Rust
+
+Protobuf and gRPC do not provide official support for Rust. Therefore, 3rd-party tools have to be
+used to generate the code. We use a custom made tool [protoc-rs](https://github.com/thinkparq/protoc-rs)
+based on [Tonic](https://docs.rs/tonic/latest/tonic/), which replaces `protoc`. It does not only
+compile the protobuf files but also generate the necessary Rust crate structure to be able to import
+and use the code as a dependency in Rust projects.
 
 # References
 
---
+++ rust/beewatch/beewatch.rs
@@ -0,0 +1,409 @@
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Event {
+    /// The BeeGFS packet format defines these as a uint16 but protobuf does not support uint16 types (<https://protobuf.dev/programming-guides/proto3/#scalar>).
+    #[prost(uint32, tag = "1")]
+    pub format_version_major: u32,
+    #[prost(uint32, tag = "2")]
+    pub format_version_minor: u32,
+    /// TODO (BF-43): This is not implemented yet in the meta service, however for now we'll have BeeWatch generate sequence IDs. Update this as needed based on final meta implementation and remove current approach in socket.go.
+    #[prost(uint64, tag = "3")]
+    pub seq_id: u64,
+    #[prost(uint32, tag = "4")]
+    pub size: u32,
+    #[prost(uint64, tag = "5")]
+    pub dropped_seq: u64,
+    #[prost(uint64, tag = "6")]
+    pub missed_seq: u64,
+    #[prost(enumeration = "event::Type", tag = "7")]
+    pub r#type: i32,
+    #[prost(string, tag = "8")]
+    pub path: ::prost::alloc::string::String,
+    #[prost(string, tag = "9")]
+    pub entry_id: ::prost::alloc::string::String,
+    #[prost(string, tag = "10")]
+    pub parent_entry_id: ::prost::alloc::string::String,
+    #[prost(string, tag = "11")]
+    pub target_path: ::prost::alloc::string::String,
+    #[prost(string, tag = "12")]
+    pub target_parent_id: ::prost::alloc::string::String,
+}
+/// Nested message and enum types in `Event`.
+pub mod event {
+    #[derive(
+        Clone,
+        Copy,
+        Debug,
+        PartialEq,
+        Eq,
+        Hash,
+        PartialOrd,
+        Ord,
+        ::prost::Enumeration
+    )]
+    #[repr(i32)]
+    pub enum Type {
+        Flush = 0,
+        Truncate = 1,
+        Setattr = 2,
+        CloseWrite = 3,
+        Create = 4,
+        Mkdir = 5,
+        Mknod = 6,
+        Symlink = 7,
+        Rmdir = 8,
+        Unlink = 9,
+        Hardlink = 10,
+        Rename = 11,
+        Read = 12,
+    }
+    impl Type {
+        /// String value of the enum field names used in the ProtoBuf definition.
+        ///
+        /// The values are not transformed in any way and thus are considered stable
+        /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+        pub fn as_str_name(&self) -> &'static str {
+            match self {
+                Type::Flush => "FLUSH",
+                Type::Truncate => "TRUNCATE",
+                Type::Setattr => "SETATTR",
+                Type::CloseWrite => "CLOSE_WRITE",
+                Type::Create => "CREATE",
+                Type::Mkdir => "MKDIR",
+                Type::Mknod => "MKNOD",
+                Type::Symlink => "SYMLINK",
+                Type::Rmdir => "RMDIR",
+                Type::Unlink => "UNLINK",
+                Type::Hardlink => "HARDLINK",
+                Type::Rename => "RENAME",
+                Type::Read => "READ",
+            }
+        }
+        /// Creates an enum from field names used in the ProtoBuf definition.
+        pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+            match value {
+                "FLUSH" => Some(Self::Flush),
+                "TRUNCATE" => Some(Self::Truncate),
+                "SETATTR" => Some(Self::Setattr),
+                "CLOSE_WRITE" => Some(Self::CloseWrite),
+                "CREATE" => Some(Self::Create),
+                "MKDIR" => Some(Self::Mkdir),
+                "MKNOD" => Some(Self::Mknod),
+                "SYMLINK" => Some(Self::Symlink),
+                "RMDIR" => Some(Self::Rmdir),
+                "UNLINK" => Some(Self::Unlink),
+                "HARDLINK" => Some(Self::Hardlink),
+                "RENAME" => Some(Self::Rename),
+                "READ" => Some(Self::Read),
+                _ => None,
+            }
+        }
+    }
+}
+/// Response messages allow the subscribers to acknowledge events they have processed and request a graceful shutdown.
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Response {
+    #[prost(uint64, tag = "1")]
+    pub completed_seq: u64,
+    #[prost(bool, tag = "2")]
+    pub shutting_down: bool,
+}
+/// Generated client implementations.
+pub mod subscriber_client {
+    #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
+    use tonic::codegen::*;
+    use tonic::codegen::http::Uri;
+    #[derive(Debug, Clone)]
+    pub struct SubscriberClient<T> {
+        inner: tonic::client::Grpc<T>,
+    }
+    impl SubscriberClient<tonic::transport::Channel> {
+        /// Attempt to create a new client by connecting to a given endpoint.
+        pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
+        where
+            D: TryInto<tonic::transport::Endpoint>,
+            D::Error: Into<StdError>,
+        {
+            let conn = tonic::transport::Endpoint::new(dst)?.connect().await?;
+            Ok(Self::new(conn))
+        }
+    }
+    impl<T> SubscriberClient<T>
+    where
+        T: tonic::client::GrpcService<tonic::body::BoxBody>,
+        T::Error: Into<StdError>,
+        T::ResponseBody: Body<Data = Bytes> + Send + 'static,
+        <T::ResponseBody as Body>::Error: Into<StdError> + Send,
+    {
+        pub fn new(inner: T) -> Self {
+            let inner = tonic::client::Grpc::new(inner);
+            Self { inner }
+        }
+        pub fn with_origin(inner: T, origin: Uri) -> Self {
+            let inner = tonic::client::Grpc::with_origin(inner, origin);
+            Self { inner }
+        }
+        pub fn with_interceptor<F>(
+            inner: T,
+            interceptor: F,
+        ) -> SubscriberClient<InterceptedService<T, F>>
+        where
+            F: tonic::service::Interceptor,
+            T::ResponseBody: Default,
+            T: tonic::codegen::Service<
+                http::Request<tonic::body::BoxBody>,
+                Response = http::Response<
+                    <T as tonic::client::GrpcService<tonic::body::BoxBody>>::ResponseBody,
+                >,
+            >,
+            <T as tonic::codegen::Service<
+                http::Request<tonic::body::BoxBody>,
+            >>::Error: Into<StdError> + Send + Sync,
+        {
+            SubscriberClient::new(InterceptedService::new(inner, interceptor))
+        }
+        /// Compress requests with the given encoding.
+        ///
+        /// This requires the server to support it otherwise it might respond with an
+        /// error.
+        #[must_use]
+        pub fn send_compressed(mut self, encoding: CompressionEncoding) -> Self {
+            self.inner = self.inner.send_compressed(encoding);
+            self
+        }
+        /// Enable decompressing responses.
+        #[must_use]
+        pub fn accept_compressed(mut self, encoding: CompressionEncoding) -> Self {
+            self.inner = self.inner.accept_compressed(encoding);
+            self
+        }
+        /// Limits the maximum size of a decoded message.
+        ///
+        /// Default: `4MB`
+        #[must_use]
+        pub fn max_decoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_decoding_message_size(limit);
+            self
+        }
+        /// Limits the maximum size of an encoded message.
+        ///
+        /// Default: `usize::MAX`
+        #[must_use]
+        pub fn max_encoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_encoding_message_size(limit);
+            self
+        }
+        pub async fn receive_events(
+            &mut self,
+            request: impl tonic::IntoStreamingRequest<Message = super::Event>,
+        ) -> std::result::Result<
+            tonic::Response<tonic::codec::Streaming<super::Response>>,
+            tonic::Status,
+        > {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::new(
+                        tonic::Code::Unknown,
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static(
+                "/beewatch.Subscriber/ReceiveEvents",
+            );
+            let mut req = request.into_streaming_request();
+            req.extensions_mut()
+                .insert(GrpcMethod::new("beewatch.Subscriber", "ReceiveEvents"));
+            self.inner.streaming(req, path, codec).await
+        }
+    }
+}
+/// Generated server implementations.
+pub mod subscriber_server {
+    #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
+    use tonic::codegen::*;
+    /// Generated trait containing gRPC methods that should be implemented for use with SubscriberServer.
+    #[async_trait]
+    pub trait Subscriber: Send + Sync + 'static {
+        /// Server streaming response type for the ReceiveEvents method.
+        type ReceiveEventsStream: tonic::codegen::tokio_stream::Stream<
+                Item = std::result::Result<super::Response, tonic::Status>,
+            >
+            + Send
+            + 'static;
+        async fn receive_events(
+            &self,
+            request: tonic::Request<tonic::Streaming<super::Event>>,
+        ) -> std::result::Result<
+            tonic::Response<Self::ReceiveEventsStream>,
+            tonic::Status,
+        >;
+    }
+    #[derive(Debug)]
+    pub struct SubscriberServer<T: Subscriber> {
+        inner: _Inner<T>,
+        accept_compression_encodings: EnabledCompressionEncodings,
+        send_compression_encodings: EnabledCompressionEncodings,
+        max_decoding_message_size: Option<usize>,
+        max_encoding_message_size: Option<usize>,
+    }
+    struct _Inner<T>(Arc<T>);
+    impl<T: Subscriber> SubscriberServer<T> {
+        pub fn new(inner: T) -> Self {
+            Self::from_arc(Arc::new(inner))
+        }
+        pub fn from_arc(inner: Arc<T>) -> Self {
+            let inner = _Inner(inner);
+            Self {
+                inner,
+                accept_compression_encodings: Default::default(),
+                send_compression_encodings: Default::default(),
+                max_decoding_message_size: None,
+                max_encoding_message_size: None,
+            }
+        }
+        pub fn with_interceptor<F>(
+            inner: T,
+            interceptor: F,
+        ) -> InterceptedService<Self, F>
+        where
+            F: tonic::service::Interceptor,
+        {
+            InterceptedService::new(Self::new(inner), interceptor)
+        }
+        /// Enable decompressing requests with the given encoding.
+        #[must_use]
+        pub fn accept_compressed(mut self, encoding: CompressionEncoding) -> Self {
+            self.accept_compression_encodings.enable(encoding);
+            self
+        }
+        /// Compress responses with the given encoding, if the client supports it.
+        #[must_use]
+        pub fn send_compressed(mut self, encoding: CompressionEncoding) -> Self {
+            self.send_compression_encodings.enable(encoding);
+            self
+        }
+        /// Limits the maximum size of a decoded message.
+        ///
+        /// Default: `4MB`
+        #[must_use]
+        pub fn max_decoding_message_size(mut self, limit: usize) -> Self {
+            self.max_decoding_message_size = Some(limit);
+            self
+        }
+        /// Limits the maximum size of an encoded message.
+        ///
+        /// Default: `usize::MAX`
+        #[must_use]
+        pub fn max_encoding_message_size(mut self, limit: usize) -> Self {
+            self.max_encoding_message_size = Some(limit);
+            self
+        }
+    }
+    impl<T, B> tonic::codegen::Service<http::Request<B>> for SubscriberServer<T>
+    where
+        T: Subscriber,
+        B: Body + Send + 'static,
+        B::Error: Into<StdError> + Send + 'static,
+    {
+        type Response = http::Response<tonic::body::BoxBody>;
+        type Error = std::convert::Infallible;
+        type Future = BoxFuture<Self::Response, Self::Error>;
+        fn poll_ready(
+            &mut self,
+            _cx: &mut Context<'_>,
+        ) -> Poll<std::result::Result<(), Self::Error>> {
+            Poll::Ready(Ok(()))
+        }
+        fn call(&mut self, req: http::Request<B>) -> Self::Future {
+            let inner = self.inner.clone();
+            match req.uri().path() {
+                "/beewatch.Subscriber/ReceiveEvents" => {
+                    #[allow(non_camel_case_types)]
+                    struct ReceiveEventsSvc<T: Subscriber>(pub Arc<T>);
+                    impl<T: Subscriber> tonic::server::StreamingService<super::Event>
+                    for ReceiveEventsSvc<T> {
+                        type Response = super::Response;
+                        type ResponseStream = T::ReceiveEventsStream;
+                        type Future = BoxFuture<
+                            tonic::Response<Self::ResponseStream>,
+                            tonic::Status,
+                        >;
+                        fn call(
+                            &mut self,
+                            request: tonic::Request<tonic::Streaming<super::Event>>,
+                        ) -> Self::Future {
+                            let inner = Arc::clone(&self.0);
+                            let fut = async move {
+                                <T as Subscriber>::receive_events(&inner, request).await
+                            };
+                            Box::pin(fut)
+                        }
+                    }
+                    let accept_compression_encodings = self.accept_compression_encodings;
+                    let send_compression_encodings = self.send_compression_encodings;
+                    let max_decoding_message_size = self.max_decoding_message_size;
+                    let max_encoding_message_size = self.max_encoding_message_size;
+                    let inner = self.inner.clone();
+                    let fut = async move {
+                        let inner = inner.0;
+                        let method = ReceiveEventsSvc(inner);
+                        let codec = tonic::codec::ProstCodec::default();
+                        let mut grpc = tonic::server::Grpc::new(codec)
+                            .apply_compression_config(
+                                accept_compression_encodings,
+                                send_compression_encodings,
+                            )
+                            .apply_max_message_size_config(
+                                max_decoding_message_size,
+                                max_encoding_message_size,
+                            );
+                        let res = grpc.streaming(method, req).await;
+                        Ok(res)
+                    };
+                    Box::pin(fut)
+                }
+                _ => {
+                    Box::pin(async move {
+                        Ok(
+                            http::Response::builder()
+                                .status(200)
+                                .header("grpc-status", "12")
+                                .header("content-type", "application/grpc")
+                                .body(empty_body())
+                                .unwrap(),
+                        )
+                    })
+                }
+            }
+        }
+    }
+    impl<T: Subscriber> Clone for SubscriberServer<T> {
+        fn clone(&self) -> Self {
+            let inner = self.inner.clone();
+            Self {
+                inner,
+                accept_compression_encodings: self.accept_compression_encodings,
+                send_compression_encodings: self.send_compression_encodings,
+                max_decoding_message_size: self.max_decoding_message_size,
+                max_encoding_message_size: self.max_encoding_message_size,
+            }
+        }
+    }
+    impl<T: Subscriber> Clone for _Inner<T> {
+        fn clone(&self) -> Self {
+            Self(Arc::clone(&self.0))
+        }
+    }
+    impl<T: std::fmt::Debug> std::fmt::Debug for _Inner<T> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{:?}", self.0)
+        }
+    }
+    impl<T: Subscriber> tonic::server::NamedService for SubscriberServer<T> {
+        const NAME: &'static str = "beewatch.Subscriber";
+    }
+}
